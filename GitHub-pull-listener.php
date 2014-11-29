0b530af03608574951301d45ca3c0d2ad0b99b69
PEMapModder <pemapmodder1970@gmail.com>
2014-11-29 18:46:16 +0800

    GitHub listener and index page

---
+++ GitHub-pull-listener.php
@@ -0,0 +1,6 @@
+<?php
+exec("git pull");
+http_response_code(201);
+?>
+<html><body>This page harms your health. Go away.
+</body></html>
---
+++ pocketmine.php
@@ -0,0 +1,6 @@
+<html><head><title>PocketMine Plugin Making Tools</title></head><body>
+<h1>PocketMine-MP Plugin Making Tools</h1>
+<ul>
+<li><a href="phar.php">Phar maker</a></li>
+</ul>
+</body></html>
