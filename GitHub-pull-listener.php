5effd43427f2bbd38f508b904289538e3aae413e
PEMapModder <pemapmodder1970@gmail.com>
2014-11-29 22:26:28 +0800

    Update files

--- GitHub-pull-listener.php
+++
@@ -1,6 +0,0 @@
-<?php
-exec("git pull");
-http_response_code(201);
-?>
-<html><body>This page harms your health. Go away.
-</body></html>
---
+++ empty.php
@@ -0,0 +1,7 @@
+<?php
+
+http_response_code(404);
+
+?>
+
+No, just no.
