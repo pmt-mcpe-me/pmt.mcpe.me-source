ae0546c39efad4e00a37bca1ab1b57f923bcf5fa
PEMapModder <pemapmodder1970@gmail.com>
2014-11-30 11:54:52 +0800

    Update pocketmine-frame-up.php

--- pocketmine-frame-up.php
+++ pocketmine-frame-up.php
@@ -1,5 +1,6 @@
 <html><head><title>PocketMine Plugin Making Tools</title></head><body>
-<p align="right"><input type="button" value="Reload content frame" onclick="parent.content.location.reload()"></p>
+<p align="right"><input type="button" value="Reload content frame" onclick="parent.content.location.reload()"><br>
+Project now open-source on <a href="https://github.com/PEMapModder/web-server-source">GitHub</a>.</p>
 <h1>PocketMine-MP Plugin Making Tools</h1>
 <ul>
 <li><a href="phar.php" target="content">Phar maker</a></li>
