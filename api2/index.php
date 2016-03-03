17cc8d5b5e18aab8f6b3a1b9afb1c3a1c51fa065
PocketKiller <pocketkiller@imagicalmine.me>
2016-03-03 23:03:33 +0300

    Update index.php

--- api2/index.php
+++ api2/index.php
@@ -35,24 +35,38 @@ if(isset($_FILES["phar"])){
 	exit;
 }
 ?>
-<html>
-<head>
+<<html><head>
 	<title>Plugin API version bumper</title>
 </head>
 <body>
 	<h1>API 2.0.0 injection</h1>
 	<p>Before using this tool, please carefully read the following cautions:</p>
 	<ul>
-		<li>This tool only <em>forces</em> the plugin to say that it supports API 2.0.0 (PHP 7 update). It will not fix the actual incompatibility issues.</li>
+		<li>This tool only <em>forces</em> the plugin to say that it supports API 2.0.0 (PHP 7 update), and optionally, blindly replaces some specific backwards-incompatible changes in the phar. It will not fix the actual incompatibility issues.</li>
 		<li>If errors happen after using phars downloaded from this page, unintsall it immediately.</li>
-		<li>Click <button onclick='document.getElementById("upload").disabled = false;'>this button</button> if you have read the above.</li>
+		<li><p onclick="document.getElementById(&quot;upload&quot;).disabled = false;">click this button you read the above.</p></li>
 	</ul>
 	<hr>
 	<form action="updated.phar" method="post" enctype="multipart/form-data">
-		<input type="file" name="phar"><br>
-		No, I don't want API <code>2.0.0</code>. I want something else: <input type="text" name="api" value="2.0.0"><br>
-		<input type="submit" id="upload" value="Inject" disabled>
+		<p><input type="file" name="phar"></p>
+		<ul>
+			<li>
+				<input type="checkbox" name="diffapi" onclick="document.getElementById(&quot;apiInput&quot;).disabled = false;"> No, I don't want API <code>2.0.0</code>.
+				I want something else: <input id="apiInput" type="text" name="api" value="2.0.0" disabled="">
+			</li>
+			<li>
+				Replace the following usage of NBT tags:
+				<ul>
+					<li><input type="checkbox" name="nbt-use" checked=""> <code>use pocketmine\nbt\tag\****Tag</code></li>
+					<li><input type="checkbox" name="nbt-fqn" checked=""> fully-qualified <code>\pocketmine\nbt\tag\****Tag</code></li>
+					<li><input type="checkbox" name="nbt-new" checked=""> <code>new ****Tag</code></li>
+					<li><input type="checkbox" name="nbt-instanceof" checked=""> <code>instanceof ****Tag</code></li>
+				</ul>
+			</li>
+		</ul>
+		<p><input type="submit" id="upload" value="Inject"></p>
 	</form>
-</body>
-</html>
 
+
+
+</body></html>
